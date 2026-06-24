aea59e417eb0ddfb9ed90533721c7725278ddea2
Juan Rafael Camasis <sonic92yeylol@gmail.com>
2026-06-24 17:20:16 +0800

    s

--- FrontEnd/FrontBibleChat/src/lib/axios.js
+++ FrontEnd/FrontBibleChat/src/lib/axios.js
@@ -4,6 +4,7 @@ const ax = axios.create({
   // Vite injects environment variables onto import.meta.env
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json", // Added the missing hyphen
     Accept: "application/json", // Highly recommended for Laravel APIs
